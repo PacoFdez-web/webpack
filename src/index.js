c7512f09380bdd26673854e36ceb88be6334d583
PakoF <paco.fdez74@gmail.com>
2020-04-26 20:30:04 +0200

    Inicio Proyecto

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// SENTENCIA IMPORT
+import { saludar } from './js/componentes.js';
+import './style.css';
+
+const nombre = 'Paco Fernández';
+saludar(nombre);
+// ERROR: No se puede usar la declaración de importación fuera de un módulo
